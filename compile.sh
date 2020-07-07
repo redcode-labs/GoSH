29c878b57cd923bb530698cdfd09baace9f9ac8b
Jean-Louis Huynen <huynenjl@gmail.com>
2020-07-07 14:52:34 +0200

    add: [build] releases folder

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 go get -u github.com/gobuffalo/packr/packr
+mkdir -p releases
 GOOS=darwin GOARCH=amd64 packr build; mv ./GoSH ./releases/darwin-GoSH
 GOOS=linux GOARCH=amd64 packr build; mv ./GoSH ./releases/linux-GoSH
 GOOS=windows GOARCH=386 packr build; mv ./GoSH.exe ./releases/GoSH.exe
