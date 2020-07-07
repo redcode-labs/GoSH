7adaba7142f44ff2864aa6304c696a6b6db756ba
Jean-Louis Huynen <huynenjl@gmail.com>
2020-07-07 14:51:05 +0200

    add: [build] compilation script

---
+++ compile.sh
@@ -0,0 +1,5 @@
+go get -u github.com/gobuffalo/packr/packr
+GOOS=darwin GOARCH=amd64 packr build; mv ./GoSH ./releases/darwin-GoSH
+GOOS=linux GOARCH=amd64 packr build; mv ./GoSH ./releases/linux-GoSH
+GOOS=windows GOARCH=386 packr build; mv ./GoSH.exe ./releases/GoSH.exe
+packr clean
